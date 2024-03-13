8ed2bc0846ee22c8223aaa1f8f3da29fc5d67dc3
Kajsa <kajsalisaasp@gmail.com>
2024-03-13 17:40:27 +0100

    Changes done to about us, not finished

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -2,7 +2,7 @@
 
 <style scoped>
 article {
-    background-color: var(--mid-beige);
+    background-color: var(--light-beige);
     height: 80%;
     display: flex;
     /* flex-direction: row; */
@@ -11,35 +11,56 @@ article {
 
 }
 
-
 .section_about {
-    background-color: var(--dark-green);
+    background-color: var(--mid-beige);
     display: flex;
     flex-direction: column;
     padding: 0;
 }
 
 .block_about {
-    /* padding: 1rem; */
-    color: var(--light-beige);
+    color: var(--dark-green);
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: center;
+    margin-bottom: 50px;
 }
 
 .block_about h1 {
     padding-bottom: 1rem;
+    margin-top: 55px;
+    font-size: 24px;
+    font-family: Anevir Next DemiBold;
+
+}
+
+b{
+    font-family: Anevir Next DemiBold;
+}
+
+.block_about p{
+    padding: 10px 00;
+}
+
+/* .img-container{
+    width: 100vw;
+    height: 60vw;
+    background-color: red;
+    margin-bottom: 75px;
 }
 
 .about_img {
     width: 100%;
-    height: auto;
-}
+    height: 100%;
+    height: fit-content;
+} */
 
 .about_text {
     padding: 3rem 10%;
 }
+
+
 
 @media screen and (min-width:650px) {
 
@@ -97,36 +118,33 @@ article {
                     </h1>
 
                     <div>
+                        <p>
                         Välkommen till vår ekologiska gård, där naturen och hållbarhet står i fokus. Företaget grundades år
                         1991 av Glenn Olsson och har sedan dess levererat närodlat och ekologiskt till sina kunder. Genom
                         ekologisk odling och
                         avståndstagande
                         från kemiska ämnen skapar vi en miljö där jorden flödar av liv.
+                        </p>
 
-                        Kretsloppsanpassning är kärnan i vårat arbete – från kompostering till återvinning, minimerar vi
+                        <p>
+                        <b>Kretsloppsanpassning</b> är kärnan i vårat arbete – från kompostering till återvinning, minimerar vi
                         avfall
                         och
                         bidrar till
                         en hållbar cykel. Energiförbrukningen optimeras genom investeringar i effektiva system, och vi
                         prioriterar
                         vattenbevarande metoder för att minimera påverkan på miljön.
+                        </p>
 
-                        Lokala samarbeten är centralt för oss för att minska vårt koldioxidavtryck. Vi är inte bara odlare;
-                        vi
-                        är
-                        förkämpar för
-                        medvetenhet och utbildning. Genom att dela kunskap om ekologisk odling hoppas vi inspirera andra att
-                        välja
-                        en
-                        mer
-                        hållbar livsstil. Vår gård är en plats där varje gröda är en handling av kärlek till planeten och
-                        varje
-                        skörd
-                        ett
-                        framsteg mot en grönare framtid.
+                        <p>
+                        Lokala samarbeten är centralt för oss för att minska vårt koldioxidavtryck. Vi är inte bara odlare; vi är förkämpar för
+                        medvetenhet och utbildning. Genom att dela kunskap om ekologisk odling hoppas vi inspirera andra att välja en mer hållbar livsstil. Vår gård är en plats där varje gröda är en handling av kärlek till planeten och varje skörd ett framsteg mot en grönare framtid.
+                        </p>
                     </div>
                 </div>
-                <img src="../assets/media/AppleGarden.jpg" alt="" class="about_img about_img_div">
+                <div class="img-container">
+                <img src="../assets/media/AppleGardenCropped.jpg" alt="" class="about_img about_img_div">
+                </div>
             </div>
         </section>
     </article>
